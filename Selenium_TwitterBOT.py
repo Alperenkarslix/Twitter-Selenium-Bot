9885f01a055af46a49e3f9417775fc1ffccea8b4
Alperen Karslı <alperenkarsliceng@gmail.com>
2023-04-17 16:36:47 +0300

    ekleme

--- Selenium_TwitterBOT.py
+++ Selenium_TwitterBOT.py
@@ -1,8 +1,6 @@
 # Gerekli kütüphaneleri yükle
 from selenium import webdriver
 import time
-from tkinter import *
-from mttkinter import mtTkinter as tk
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 # Ana Pencere
@@ -177,90 +175,3 @@ def tweetiretweetet(kullaniciadi,sifre,kullanici, tweet):
     driver.find_element(by="xpath", value="//body/div[@id='react-root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]").click()
     time.sleep(3)
     
-if __name__ == "__main__":
-
-    def tweetatma():
-        tweetat(kullaniciaditut.get(), sifretut.get(), tweettut.get())
-    def takipatma():
-        takipat(kullaniciaditut.get(), sifretut.get(), takipicinkullaniciaditut.get())
-    def sikayetetme():
-        sikayetet(kullaniciaditut.get(), sifretut.get(), sikayeticinkullaniciaditut.get())
-    def ilktweetelikeatma():
-        ilktweetelikeat(kullaniciaditut.get(), sifretut.get(), likeicinkullaniciaditut.get())
-    def tweetelikeatma():
-        tweetelikeat(kullaniciaditut.get(), sifretut.get(), tweetlikeicinkullaniciaditut.get(), tweetlikeicinidtut.get() )
-    def tweetiretweetetma():
-        tweetiretweetet(kullaniciaditut.get(), sifretut.get(), tweetrticinkullaniciaditut.get(), tweetrticinidtut.get() )
-    window = Tk()
-    window.title("Alperen Twitter BOT")
-    lbl = Label(window, text="Kullanıcı Adı:")
-    lbl2 = Label(window, text="Şifre:")
-    lbl.grid(column=0, row=0)
-    lbl2.grid(column=2, row=0)
-    kullaniciaditut = tk.StringVar(window)
-    kullaniciadigir = Entry(window,textvariable = kullaniciaditut, width=10)
-    kullaniciadigir.grid(column=1, row=0)
-    sifretut = tk.StringVar(window)
-    sifregir = Entry(window,textvariable = sifretut, width=10, show="*")
-    sifregir.grid(column=3, row=0)
-    lbl3 = Label(window, text="İşlem Seçiniz:")
-    lbl3.grid(column=0, row=1)
-    lbl4 = Label(window, text="Tweet:")
-    lbl4.grid(column=0, row=2)
-    tweettut =tk.StringVar(window)
-    txt3 = Entry(window,textvariable = tweettut, width=10)
-    txt3.grid(column=1, row=2)
-    btn2 = Button(window, text="Tweet At", command=tweetatma)
-    btn2.grid(column=2, row=2)
-
-    lbl5 = Label(window, text="Kullanıcı Adı:")
-    lbl5.grid(column=0, row=3)
-    takipicinkullaniciaditut =tk.StringVar(window)
-    txt3 = Entry(window,textvariable=takipicinkullaniciaditut, width=10)
-    txt3.grid(column=1, row=3)
-    btn3 = Button(window, text="Takip Et", command=takipatma)
-    btn3.grid(column=2, row=3)
-
-    lbl6 = Label(window, text="Kullanıcı Adı:")
-    lbl6.grid(column=0, row=4)
-    sikayeticinkullaniciaditut =tk.StringVar(window)
-    txt4 = Entry(window,textvariable=sikayeticinkullaniciaditut, width=10)
-    txt4.grid(column=1, row=4)
-    btn4 = Button(window, text="Şikayet Et", command=sikayetetme)
-    btn4.grid(column=2, row=4)
-
-    lbl7 = Label(window, text="Kullanıcı Adı:")
-    lbl7.grid(column=0, row=5)
-    likeicinkullaniciaditut =tk.StringVar(window)
-    txt5 = Entry(window,textvariable=likeicinkullaniciaditut, width=10)
-    txt5.grid(column=1, row=5)
-    btn5 = Button(window, text="İlk Tweete Like At", command=ilktweetelikeatma)
-    btn5.grid(column=2, row=5)
-
-    lbl8 = Label(window, text="Kullanıcı Adı:")
-    lbl8.grid(column=0, row=6)
-    tweetlikeicinkullaniciaditut =tk.StringVar(window)
-    txt6 = Entry(window,textvariable=tweetlikeicinkullaniciaditut, width=10)
-    txt6.grid(column=1, row=6)
-    lbl9 = Label(window, text="Tweet ID:")
-    lbl9.grid(column=2, row=6)
-    tweetlikeicinidtut =tk.StringVar(window)
-    txt7 = Entry(window,textvariable=tweetlikeicinidtut, width=10)
-    txt7.grid(column=3, row=6)
-    btn6 = Button(window, text="Tweete Like At", command=tweetelikeatma)
-    btn6.grid(column=4, row=6)
-
-    lbl9 = Label(window, text="Kullanıcı Adı:")
-    lbl9.grid(column=0, row=7)
-    tweetrticinkullaniciaditut =tk.StringVar(window)
-    txt7 = Entry(window,textvariable=tweetrticinkullaniciaditut, width=10)
-    txt7.grid(column=1, row=7)
-    lbl10 = Label(window, text="Tweet ID:")
-    lbl10.grid(column=2, row=7)
-    tweetrticinidtut =tk.StringVar(window)
-    txt8 = Entry(window,textvariable=tweetrticinidtut, width=10)
-    txt8.grid(column=3, row=7)
-    btn7 = Button(window, text="Tweeti Retweet Et", command=tweetiretweetetma)
-    btn7.grid(column=4, row=7)
-    window.geometry('700x300')
-    window.mainloop()
